bb7bb577ba410b117321259be07acecfc4d4e348
Balogh Barnabás <balogh.barnabas2008@gmail.com>
2026-03-21 19:04:40 +0100

    Initial commit

---
+++ Orchestrator/OrchestratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrchestratorApp.swift
+//  Orchestrator
+//
+//  Created by Balogh Barnabás on 2026. 03. 21..
+//
+
+import SwiftUI
+
+@main
+struct OrchestratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OrchestratorTests/OrchestratorTests.swift
@@ -0,0 +1,17 @@
+//
+//  OrchestratorTests.swift
+//  OrchestratorTests
+//
+//  Created by Balogh Barnabás on 2026. 03. 21..
+//
+
+import Testing
+@testable import Orchestrator
+
+struct OrchestratorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
